13b60303c31d4a174c3e8261136c1df37c5ad17d
Riccardo Delli Compagni <47278210+RiccardoDC@users.noreply.github.com>
2023-07-13 14:13:34 +0200

    Add app.py file

---
+++ app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import openai 
+from PIL import Image
+
+
+
+#st.title('My app')
+
